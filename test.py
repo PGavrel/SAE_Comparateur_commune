26925944c1f8b724dab3630158883dd7c20a637b
PGavrel <gpierre2603@gmail.com>
2025-04-24 12:10:04 +0200

    Update test.py

--- test.py
+++ test.py
@@ -167,9 +167,9 @@ def get_blason_et_site_via_api(nom_ville):
     if nom_ville.lower() == "paris":
         return {
             "ville": "Paris",
-            "blason_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Grandes_Armes_de_Paris.svg/langfr-250px-Grandes_Armes_de_Paris.svg.png",
+            "blason_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Blason_Paris_final.png/800px-Blason_Paris_final.png",
             "site_web": "https://www.paris.fr",
-            "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/330px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg"
+            "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/1920px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg"
         }
 
     try:
